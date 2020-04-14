158b09fcf84cd32ed5bdd57f15434d6897f09ce7
E. Gökhan Karahan <egkarahan@hotmail.com>
2020-04-14 23:07:20 +0300

    TransferMicroservice is fillter and completed consuming messages

---
+++ MicroRabbit/MicroRabbit.Transfer.API/Controllers/TransferContoller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MicroRabbit.Transfer.Application.Interfaces;
+using MicroRabbit.Transfer.Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace MicroRabbit.Transfer.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TransferContoller : ControllerBase
+    {
+       
+        private readonly ILogger<TransferContoller> _logger;
+        private readonly ITransferService _transferService;
+
+        public TransferContoller(ILogger<TransferContoller> logger , ITransferService transferService)
+        {
+            _logger = logger;
+            _transferService = transferService;
+        }
+
+        [HttpGet]
+        public ActionResult<List<TransferLog>> Get()
+        {
+            return Ok(_transferService.GetTransferLogs());
+        }
+    }
+}
